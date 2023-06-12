aeae4c4f34d8678fc0183b855a6400695af7078d
Emanuel Rampinelli <37427276+emanuelrampinelli@users.noreply.github.com>
2023-06-12 17:02:40 -0300

    [FIX] Conexao BD

--- src/main/java/com/talent/model/Cargo.java
+++ src/main/java/com/talent/model/Cargo.java
@@ -1,8 +1,17 @@
 package com.talent.model;
 
+import lombok.Data;
+import javax.persistence.*;
+
+@Entity
+@Table(name="CARGO")
+@Data
 public class Cargo {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    @Column(nullable = false)
     private String nome;
-    private String descricao;
 }
